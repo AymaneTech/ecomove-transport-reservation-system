8abe4d68e4ebb2afcbe1686ab98d3ee367882228
geek <elmainiaymane03@gmail.com>
2024-08-28 16:14:21 +0100

    setup: setup java project
    add my framework dependency

---
+++ src/main/java/com/wora/App.java
@@ -0,0 +1,17 @@
+package com.wora;
+
+import ma.codex.Framework.Kernel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Kernel.run(App.class);
+
+        System.out.println("hello world");
+    }
+}
