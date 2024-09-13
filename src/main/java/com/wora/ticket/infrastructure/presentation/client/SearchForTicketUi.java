8a68d2852488be63304ee711deccfab87032e9a9
geek <elmainiaymane03@gmail.com>
2024-09-13 10:57:11 +0100

    renmae ui of administration package

--- src/main/java/com/wora/ticket/infrastructure/presentation/client/SearchForTicketUi.java
+++ src/main/java/com/wora/ticket/infrastructure/presentation/client/SearchForTicketUi.java
@@ -1,4 +1,26 @@
 package com.wora.ticket.infrastructure.presentation.client;
 
+import com.wora.ticket.application.services.SearchForTicketService;
+
+import java.time.LocalDate;
+
+import static com.wora.common.utils.InputScanner.scanLocalDate;
+import static com.wora.common.utils.InputScanner.scanString;
+
 public class SearchForTicketUi {
+    private final SearchForTicketService searchForTicketService;
+
+    public SearchForTicketUi(SearchForTicketService searchForTicketService) {
+        this.searchForTicketService = searchForTicketService;
+    }
+
+    public void search() {
+        System.out.println("Please to enter all the information to do your search");
+
+        final String startStation = scanString("Please to enter the name of city you are in: ");
+        final String endStation = scanString("Please to enter the name of city you are out: ");
+        final LocalDate startDate = scanLocalDate("Please to enter the date you are in: ");
+
+
+    }
 }
